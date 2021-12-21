40619d8a31f0719db0c9998a4ecf64c53bdcdfbc
Rajkrithika <mail2krithika1@gmail.com>
2021-12-20 19:29:46 -0500

    first commit

---
+++ 01-Fundamentals-Part-1/starter/assignement.js
@@ -0,0 +1,12 @@
+const country = "india";
+const continent = "asia";
+let population = 3000000;
+let isIsland = true;
+let language;
+language = "tamil" ;
+console.log(population);
+console.log(country);
+console.log(continent);
+console.log(language);
+console.log(isIsland);
+
