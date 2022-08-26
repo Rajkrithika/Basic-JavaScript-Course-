4ebd68fed5118b715659488a2e73f1d1095b42ff
Rajkrithika <mail2krithika1@gmail.com>
2022-08-26 12:15:07 -0400

    learned array,object and loops

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,130 @@
+  let js = 'amazing';
+  40-7+2;
+  console.log(40-7+2);
+  console.log('jonas');
+  console.log(23);
+
+  let firstName = "bob";
+  console.log(firstName);
+  console.log(firstName);
+let secondName = "krithika";
+let second;
+console.log(secondName);
+
+let myFirstJob = "programmer";
+let myCurrebtJob = "Teacher";
+console.log(myFirstJob);
+
+let country = "India";
+let continent = "Asia";
+let population = "12387903";
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIslandIndia = true;
+let language;
+console.log(language);
+console.log(typeof isIslandIndia );
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+
+/*
+let markWeight = 78;
+let markHeight = 1.69;
+let johnWeight = 92;
+let johnHeight = 1.95;
+let markBMI = 78/(1.69*1.69);
+let johnBMI = 92/(1.95*1.95);
+let markHigherBMI = markBMI > johnBMI ;
+console.log(markHigherBMI);
+*/
+ let markWeight = 95;
+ let markHeight = 1.99;
+ let johnWeight = 85;
+ let johnHeight = 1.76;
+ let markBMI = 95/(1.99*1.99);
+ let johnBMI = 85/(1.76*1.76);
+ let markHigherBMI = markBMI > johnBMI;
+ console.log(markHigherBMI);
+ console.log(markBMI , johnBMI);
+
+if(markHigherBMI){
+console.log(`mark's BMI ${markBMI} is higher than johnBMI ${johnBMI}`);
+}
+else {
+  console.log(`john's BMI ${johnBMI} is higher than markBMI ${markBMI}`);
+}
+
+const inputYear = `1992`;
+console.log(Number(inputYear)+20);
+
+console.log(`I am`+ 23 + `years old`);
+console.log(`23` / `2`);
+console.log(`23`+`10`-`1`);
+/*
+
+const day = `monday`:
+
+switch(day) {
+  case `monday`:
+  console.log(`plan the course`);
+  break;
+  case `tuesday`:
+  console.log(`write the code`);
+  break;
+  case ` wednsday`:
+  console.log(`enjy the day`);
+  break;
+  case `thrusday`:
+  console.log(`record videos`);
+  break;
+}
+*/
+/*
+function logger() {
+  console.log(`my name is jonas`);
+}
+logger();
+
+function student(){
+  console.log(`my name is krithika`);
+}
+student();
+
+
+
+function fruitProcessor(apple,orange){
+  console.log(apple,orange);
+  const juice = `juice with ${apple} apple and ${orange}
+orange.`;
+return juice;
+}
+fruitProcessor(5,0);
+
+function getAge(age){
+  console.log(age);
+  const newAge = `my age is ${age} age`;
+  return newAge;
+}
+const myGrandFatherAge = getAge(64);
+const mySonAge = getAge(12);
+console.log(myGrandFatherAge);
+console.log(mySonAge);
+
+function calcAge1(birthyear){
+console.log(2020-birthyear);
+  return 2020 - birthyear;
+}
+calcAge1(1992);
+*/
+
+
+
+
+
+
+const friends = ['mike','raj','krithika'];
+friends.push('jkjk');
+console.log(friends);
---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,59 @@
+// const veggies1 = 'mint';
+// const veggies2 = 'carrot';
+// const veggies3 = 'spinach';
+//
+// const vegatables = ['mint', 'carrot', 'spinach','tomato', 6, 1000];
+// console.log(vegatables);
+// vegatables.push('beetroot', 'apple');
+// console.log(vegatables);
+// vegatables.unshift('beetroot');
+// console.log(vegatables);
+// vegatables.pop();
+// console.log(vegatables);
+// console.log(vegatables.indexOf('spinach'));
+// console.log(vegatables.includes('kkkkkk'));
+//
+// if(vegatables.includes('terrr')){
+//   console.log("happy tomato");
+// }
+// else {
+// console.log("Sad tomato");
+// }
+
+
+// function CalcTip (bills){
+//   let tips = [];
+// let total = []
+// bills.forEach((bill, i) => {
+// let tip = 0;
+//      if ((bill >= 50) && (bill<=300)){
+//        tip = (bill /100)*15;
+//     } else {
+//     tip = (bill /100)*20;
+//   }
+//   total.push(bill+tip);
+//   tips.push(tip);
+// })
+//
+// console.log(total);
+// console.log(tips);
+// return tips;
+// }
+// const bills = [125, 555, 44];
+// CalcTip(bills);
+//
+
+
+let fruits = {
+  apples : 3,
+  orange : 8,
+  jackfruit : 7,
+  fruitsName : "fruit 1",
+  fruitsName2 : "fruit 2",
+  basket: [43,"name"]
+};
+console.log(fruits);
+console.log(fruits.orange);
+console.log(fruits['fruitsName']);
+fruits.names = "karnan";
+console.log(fruits);
